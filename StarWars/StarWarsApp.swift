677d5b0ddc54376dc420c28556b3c26644b4dc1a
Devashree KS <devashree.ks@robosoftin.com>
2022-01-22 21:13:01 +0530

    Initial Commit

---
+++ StarWars/StarWarsApp.swift
@@ -0,0 +1,20 @@
+//
+//  StarWarsApp.swift
+//  StarWars
+//
+//  Created by Devashree KS on 22/01/22.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
